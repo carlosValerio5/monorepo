8602869be07fbd2e4c5f6127b5abc51272216fe7
carlosValerio5 <carlitomanuelvalri@hotmail.com>
2025-06-25 23:57:59 -0600

    test 5

--- apps/nest_test/src/app/app.service.ts
+++ apps/nest_test/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello World! attempt 4' };
+    return { message: 'Hello World! attempt 5' };
   }
 }
